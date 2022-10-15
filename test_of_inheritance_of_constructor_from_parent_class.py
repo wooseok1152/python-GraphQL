3567d152055e1fb9e37247f31b5595c25c727b1f
choi <choi@bzznbyd.com>
2022-10-15 11:23:08 +0900

    add comments for inheritance

--- test_of_inheritance_of_constructor_from_parent_class.py
+++ test_of_inheritance_of_constructor_from_parent_class.py
@@ -4,11 +4,11 @@ class test :
 
         self.name = name
 
-class Person(test) :
+class Person(test) :        # 'test'부모 클래스를 상속했기 때문에, test 클래스의 생성자 함수도 상속됨
 
     def print_name(self) :
 
         return self.name
 
-a = Person("choi")
+a = Person("choi")          # 'test'클래스의 생성자 함수가 호출됨
 print(a.print_name())
